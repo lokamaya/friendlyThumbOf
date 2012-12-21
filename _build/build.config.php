b51a7fd439210f03616b40b2b9677490284fbd3d
Zaenal Muttaqin <public@lokamaya.com>
2012-12-21 21:54:46 +0700

    mis-type comment

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,12 +4,12 @@
  *
  * Copyright 2012 by Zaenal <@lokamaya>
  *
- * Catalog is free software; you can redistribute it and/or modify it under the
+ * friendlyThumbOf is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * Catalog is distributed in the hope that it will be useful, but WITHOUT ANY
+ * friendlyThumbOf is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
